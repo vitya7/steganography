6f89b4792a2a03f1dc6bb2cad7addd6f0818e86a
vitya7 <31449667+vitya7@users.noreply.github.com>
2019-03-10 00:25:39 +0400

    add catch brunch for file_type_exception, add local testing

--- main.cpp
+++ main.cpp
@@ -5,14 +5,73 @@ using namespace cmd_utility;
 
 using namespace std;
 
+void test();
+
 int main (int argn, char **args)
 {
     try
     {
-        auto program = program_config {argn, args}.get_program();
+        auto program = program_config {size_t( argn ), args}.get_program();
         program();
     }
     catch (invalid_argument const& ex)
+    {
+        cerr << ex.what();
+    }
+    catch (file_utility::file_type_exception const& ex)
+    {
+        cerr << ex.what();
+    }
+    catch (exception const& ex)
+    {
+        cerr << "invalid command";
+    }
+}
+
+void test ()
+{
+    vector <string> cmd1
+    {   "heh"
+    ,   "-encode"
+    ,   "-i",   "data/tiger.bmp"
+    ,   "-o",   "data/tiger_o.bmp"
+    ,   "-t",   "lol kek heh loh "
+    };
+
+    vector <string> cmd2
+    {   "heh"
+    ,   "-decode",  "t"
+    ,   "-i",       "data/tiger_o.bmp"
+    };
+
+    vector <string> cmd3
+    {   "heh"
+    ,   "-encode"
+    ,   "-i",       "data/zmap.bmp"
+    ,   "-o",       "data/zmap_en.bmp"
+    ,   "-src",     "data/zkars.jpg"
+    ,   "-bits",    "1"
+    };
+
+    vector <string> cmd4
+    {   "heh"
+    ,   "-decode",  "src"
+    ,   "-i",       "data/zmap_en.bmp"
+    ,   "-o",       "data/zkars_de.bmp"
+    ,   "-bits",    "1"
+    };
+
+
+    try
+    {
+        program_config {cmd3.size(), cmd3.begin()}.get_program() ();
+        program_config {cmd4.size(), cmd4.begin()}.get_program() ();
+    }
+    catch (invalid_argument const& ex)
+    {
+        cerr << ex.what();
+    }
+    catch (file_utility::file_type_exception const& ex)
     {
         cerr << ex.what();
     }
